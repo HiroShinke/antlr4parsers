ab76f6c41150b0a7c84044d04072947697590660
Hirofumi SHINKE <hiro@molloy.local>
2021-07-27 07:47:06 +0900

    new junit style

--- src/test/java/com/github/hiroshinke/antlrsample/GrunUtilTest.java
+++ src/test/java/com/github/hiroshinke/antlrsample/GrunUtilTest.java
@@ -2,37 +2,19 @@
 
 package com.github.hiroshinke.antlrsample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class GrunUtilTest
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GrunUtilTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( GrunUtilTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
 

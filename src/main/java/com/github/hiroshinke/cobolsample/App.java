edb1da64e7638dc4dbf467692685ee5136189876
Hirofumi SHINKE <hiro@molloy.local>
2021-08-12 00:08:06 +0900

    work for file, directories...

--- src/main/java/com/github/hiroshinke/cobolsample/App.java
+++ src/main/java/com/github/hiroshinke/cobolsample/App.java
@@ -21,29 +21,103 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import static com.github.hiroshinke.cobolsample.AntlrUtil.*;
 
 
 class App {
 
+
+    interface Consumer<T> {
+	void accept(T t) throws Exception;
+    }
+    
     public static void main(String[] args) throws Exception {
+
         // create a CharStream that reads from standard input
 
-	InputStream is = System.in;
-
 	if( args.length > 0 ){
+
 	    String filePath = args[0];
 	    File fileInput = new File(filePath);
-	    is = new FileInputStream(fileInput);
+
+	    doFile(fileInput,(file) -> {
+		    InputStream is = toSrcStream(new FileInputStream(file));
+		    Cobol85Parser parser = createParser(is);
+		    printCallInfo(file.toString(),parser);
+		    printMoveInfo(file.toString(),parser);
+		    printDataDescriptionInfo(file.toString(),parser);
+		});
+
+	} else {
+
+	    Cobol85Parser parser = createParser(System.in);
+	    
+	    printCallInfo("<stdin>",parser);
+	    printMoveInfo("<stdin>",parser);
+	    printDataDescriptionInfo("<stdin>",parser);
 	}
+    }
 
-	Cobol85Parser parser = createParser(is);
-	// System.out.println(tree.toStringTree(parser));
+    public static void doFile(File file, Consumer<File> proc) throws Exception {
 
-	printCallInfo(parser);
-	printMoveInfo(parser);
-	printDataDescriptionInfo(parser);
+	if( file.isDirectory() ){
+	    doDir(file,proc);
+	}
+	else {
+	    try {
+		System.out.println("process file: " + file.toString() );
+		proc.accept(file);
+	    } catch( Exception e ){
+		e.printStackTrace();
+		throw e;
+	    }
+	}
+    }
+
+    public static void doDir(File file, Consumer<File> proc) throws Exception {
+
+	for(File f: file.listFiles() ){
+	    doFile(f,proc);
+	}
+    }
+
+
+    static Pattern pattern = Pattern.compile("^.{6}-\\s+(\"|\')");
+
+    public static InputStream toSrcStream(InputStream is) throws Exception {
+
+	BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+	StringBuffer buff = new StringBuffer();
+
+	String line;
+	while( (line = rd.readLine()) != null ){
+	    if( line.charAt(6) == ' ' ){
+		buff.append(line.substring(7,72));
+		buff.append('\n');
+	    }
+	    else if( line.charAt(6) == '-' ){
+		int last = buff.length();
+		buff.delete(last-1,last);
+		Matcher m = pattern.matcher(line);
+		if( m.find() ){
+		    int e = m.end();
+		    buff.append(line.substring(e,72));
+		} else {
+		    buff.append(line.substring(7,72));
+		}
+		buff.append('\n');
+	    }
+	}
+	return new ByteArrayInputStream(buff.toString().
+					getBytes(StandardCharsets.UTF_8));
     }
 
     public static Cobol85Parser createParser(InputStream is) throws Exception {
@@ -56,30 +130,32 @@ class App {
 	return parser;
     }
 
-    static void printMoveInfo(Cobol85Parser parser){
+    static void printMoveInfo(String file, Cobol85Parser parser){
 
 	parser.reset();
         ParseTree tree = parser.startRule();
+	Collection<ParseTree> moves = XPath.findAll(tree,
+						    "//moveStatement/*",
+						    parser);
+	for( ParseTree m : moves ){
 
-	String xpath = "//moveStatement"; // get children of blockStatement
-	String treePattern = "MOVE <foo:identifier> TO <goo:identifier>";
-	ParseTreePattern p =
-	    parser.compileParseTreePattern(
-			treePattern,   
-			Cobol85Parser.RULE_moveStatement
-					  );
-	List<ParseTreeMatch> matches = p.findAll(tree, xpath);
-	System.out.println(matches);
-	for( ParseTreeMatch m : matches ){
-	    String from = m.get("foo").getText();
-	    String to = m.get("goo").getText();
-	    System.out.printf("move : %s -> to : %s\n", from ,to );
+	    String from = xpathSubTreeText
+		(
+		 parser,m,
+		 List.of("//moveToSendingArea","//moveCorrespondingToSendingArea")
+		 );
 
+	    Collection<ParseTree> toes = xpathSubTrees(parser,m,"*/identifier");
+	    for( ParseTree t : toes ){
+		System.out.printf("moveStatement %s,%s,%s\n",
+				  file,
+				  from ,
+				  t.getText() );
+	    }
 	}
-
     }
 
-    static void printCallInfo(Cobol85Parser parser){
+    static void printCallInfo(String file, Cobol85Parser parser){
 
 	parser.reset();
         ParseTree tree = parser.startRule();
@@ -101,14 +177,15 @@ class App {
 		     (
 		      parser,t,"*//callByReference",
 		      (p) -> {
-			  System.out.printf("call : %s using : %s\n",
+			  System.out.printf("call : %s, %s, %s\n",
+					    file,
 					    callName ,
 					    p.getText() );
 		      });
 	     });
     }
 
-    static void printDataDescriptionInfo(Cobol85Parser parser){
+    static void printDataDescriptionInfo(String file, Cobol85Parser parser){
 
 	parser.reset();
         ParseTree tree = parser.startRule();
@@ -133,6 +210,7 @@ class App {
 	    String value = xpathSubTreeText(parser,e,"*//dataValueIntervalFrom");
 
 	    System.out.println("dataDescription: " + String.join(",",
+								 file,
 								 level,
 								 name,
 								 pict,

fd2a67e3c364a8e9fac98c4838e595b6db69606b
Hirofumi SHINKE <hiro@molloy.local>
2021-09-26 22:02:28 +0900

    *** empty log message ***

--- src/test/sh/test2.sh
+++ src/test/sh/test2.sh
@@ -2,9 +2,8 @@
 
 CLASSPATH=target/classes:~/.m2/repository/org/antlr/antlr4-runtime/4.9/antlr4-runtime-4.9.jar:/Users/hiro/.m2/repository/commons-cli/commons-cli/1.4/commons-cli-1.4.jar
 
-java com.github.hiroshinke.cobolsample.App -s XXX.COB
 
-
+java com.github.hiroshinke.cobolsample.App -s NIST/COBOL -I NIST/CLBRY
 
 
 
---
+++ src/test/sh/testvb.sh
@@ -0,0 +1,3 @@
+
+CLASSPATH=target/classes:/Users/hiro/.m2/repository/org/antlr/antlr4-runtime/4.9/antlr4-runtime-4.9.jar:/Users/hiro/.m2/repository/commons-cli/commons-cli/1.4/commons-cli-1.4.jar
+java com.github.hiroshinke.vbsample.App $*
